8a42a826539008cceebd8ceb1e27bdb5fce714ee
tester <cho0303552@naver.com>
2023-07-06 11:34:43 +0900

    modified data

--- song/database.py
+++ song/database.py
@@ -1,9 +1,143 @@
+import pandas as pd
 import sqlite3
 
 class DataBase:
     def __init__(self):
+        # 데이터 불러오기
+        """
+        self.common_data = pd.read_csv("./data/common_data.csv")
+        self.accommodation_sub_data = pd.read_csv("./data/accommodation_sub_data.csv")
+        self.restaurant_sub_data = pd.read_csv("./data/restaurant_sub_data.csv")
+        self.trip_place_sub_data = pd.read_csv("./data/trip_place_sub_data.csv")
+
         self.conn = sqlite3.connect("main_db")
         self.cur = self.conn.cursor()
-        self.conn.execute('CREATE TABLE IF NOT EXISTS main_table(id integer PRIMARY KEY AUTOINCREMENT, borough text, vacation_spot text, latitude float, longitude float, location_address text, call_address text, homepage text, day_off text, work_time text, convenience text )')
+        self.conn.execute('CREATE TABLE IF NOT EXISTS common'
+                          '(id varchar(255) PRIMARY KEY,'
+                          'brought varchar(255), '
+                          'name varchar(255), '
+                          'address varchar(255),'
+                          'latitude float, '
+                          'longitude float, '
+                          'contact_number varchar(255))')
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS trip_place_sub'
+                          '(id varchar(255), '
+                          'detail text,'
+                          'image_address text, '
+                          'FOREIGN KEY (id) REFERENCES common (id))')
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS restaurant_sub'
+                          '(id varchar(255), '
+                          'detail text,'
+                          'image_address text, '
+                          'FOREIGN KEY (id) REFERENCES common (id))')
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS accommodation_sub'
+                          '(id varchar(255), '
+                          'detail text,'
+                          'image_address text, '
+                          'FOREIGN KEY (id) REFERENCES common (id))')
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS recommend'
+                          '(id varchar(255), '
+                          'name text, '
+                          'ten integer, '
+                          'twenty integer, '
+                          'thirty integer, '
+                          'fourty integer, '
+                          'fifty integer, '
+                          'sixty integer, '
+                          'FOREIGN KEY (id) REFERENCES common (id))')
+
+        # 'gangwon integer, '
+        # 'gyeonggi integer, '
+        # 'geongnam integer, '
+        # 'geongbuk integer, '
+        # 'daegu integer, '
+        # 'seoul integer, '
+        # 'sejong integer, '
+        # 'ulsan integer, '
+        # 'incheon integer, '
+        # 'jeonnam integer, '
+        # 'jeonbuk integer, '
+        # 'jeju integer, '
+        # 'chungnam integer, '
+        # 'chungbuk integer, '
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS member'
+                          '(user_id varchar(255) PRIMARY KEY,'
+                          'password varchar(255), '
+                          'name varchar(255), '
+                          'age integer)')
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS schedule'
+                          '(user_id varchar(255), '
+                          'order_number integer, '
+                          'contents_id varchar(255), '
+                          'contents_name varchar(255), '
+                          'contents_latitude float, '
+                          'contents_longitude float, '
+                          'image_address text, '
+                          'FOREIGN KEY (user_id) REFERENCES member (user_id), '
+                          'FOREIGN KEY (contents_id) REFERENCES common (id))')
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS metro'
+                          '(station_id varchar(255) PRIMARY KEY, '
+                          'line_number varchar(255), '
+                          'station_name varchar(255), '
+                          'station_address varchar(255), '
+                          'station_latitude float, '
+                          'station_longitude float, '
+                          'between_distance float, '
+                          'between_time float)')
+
+        # 데이터 넣기.
+        self.conn = sqlite3.connect("main_db")
+        self.cur = self.conn.cursor()
+
+        self.common_data = pd.read_csv("./data/common_data.csv")
+        for index, row in self.common_data.iterrows():
+            insert_data = list(row.values)
+            self.cur.execute('INSERT INTO common(id, brought, name, address, latitude, longitude, contact_number) VALUES(?, ?, ?, ?, ?, ?, ?)', insert_data)
+
+        self.accommodation_sub_data = pd.read_csv("./data/accommodation_sub_data.csv")
+        for index, row in self.accommodation_sub_data.iterrows():
+            insert_data = list(row.values)
+            self.cur.execute(
+                'INSERT INTO accommodation_sub(id, detail, image_address) VALUES(?, ?, ?)', insert_data)
+            
+        self.restaurant_sub_data = pd.read_csv("./data/restaurant_sub_data.csv")
+        for index, row in self.restaurant_sub_data.iterrows():
+            insert_data = list(row.values)
+            self.cur.execute(
+                'INSERT INTO restaurant_sub(id, detail, image_address) VALUES(?, ?, ?)', insert_data)
+            
+        self.trip_place_sub_data = pd.read_csv("./data/trip_place_sub_data.csv")
+        for index, row in self.trip_place_sub_data.iterrows():
+            insert_data = list(row.values)
+            self.cur.execute(
+                'INSERT INTO trip_place_sub(id, detail, image_address) VALUES(?, ?, ?)', insert_data)
+
+        self.recommend_data = pd.read_csv("./data/recommend_data.csv")
+        for index, row in self.recommend_data.iterrows():
+            insert_data = list(row.values)
+            self.cur.execute('INSERT INTO recommend(id, name, ten, twenty, thirty, fourty, fifty, sixty) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', insert_data)
+
+        self.conn.commit()
+        self.conn.close()
+        """
+
+        # 로드 테스트
+        self.conn = sqlite3.connect("main_db")
+        self.conn.execute('PRAGMA foreign_keys = on')
+        self.cur = self.conn.cursor()
+        self.cur.execute('SELECT * from common inner join trip_place_sub on common.id == trip_place_sub.id')
+        rows = self.cur.fetchall()
+        for row in rows:
+            print(row)
 
 
+
+if __name__ == "__main__":
+    data_base_object = DataBase()
---
+++ song/table_data_preprocessing.py
@@ -0,0 +1,137 @@
+import pandas as pd
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
+
+# 전처리 결과를 Database에 넣는다.
+class PreprocessTable:
+    def __init__(self):
+        # self.accommodation_data = pd.read_csv("./data/accommodation_data.csv", encoding='cp949')
+        # self.accommodation_id_data = None
+        # self.restaurant_data = pd.read_csv("./data/restaurant_data.csv", encoding='cp949')
+        # self.restaurant_id_data = None
+        # print(self.restaurant_data)
+        # self.tourist_attractions_data = pd.read_csv("./data/tourist_attractions_data.csv", encoding="cp949")
+        # self.tourist_attractions_id_data = None
+        # print(self.tourist_attractions_data)
+
+        # a1 ....
+        # r1 ....
+        # t1 ....
+        # 에 해당되는 id값을 넣을 예정
+        # 이미지 경로 추가
+
+        # 숙박업 id 넣기
+        # number_list = list()
+        # for i in range(len(self.accommodation_data)):
+        #     number_list.append(f"a{i + 1}")
+        # self.accommodation_id_data = pd.DataFrame(number_list, columns=["id"])
+        # self.accommodation_data = pd.concat([self.accommodation_id_data, self.accommodation_data], axis=1)
+        # self.accommodation_data.to_csv("./data/accommodation_data.csv", index=False)
+
+        # 음식점 id 넣기
+        # number_list = list()
+        # for i in range(len(self.restaurant_data)):
+        #     number_list.append(f"r{i + 1}")
+        # self.restaurant_id_data = pd.DataFrame(number_list, columns=["id"])
+        # self.restaurant_data = pd.concat([self.restaurant_id_data, self.restaurant_data], axis=1)
+        # self.restaurant_data.to_csv("./data/restaurant_data.csv", index=False)
+        # print(self.restaurant_data)
+
+        # 관광지 id 넣기
+        # number_list = list()
+        # for i in range(len(self.tourist_attractions_data)):
+        #     number_list.append(f"t{i + 1}")
+        # self.tourist_attractions_id_data = pd.DataFrame(number_list, columns=["id"])
+        # self.tourist_attractions_data = pd.concat([self.tourist_attractions_id_data, self.tourist_attractions_data], axis=1)
+        # self.tourist_attractions_data.to_csv("./data/tourist_attractions_data.csv", index=False)
+        # print(self.tourist_attractions_data)
+
+        # ====================================================================================================================
+
+        # 이미지 주소 값 넣기
+        # accommodation_file_location = "C:/Users/KDT02/Desktop/new_team_project_1/image/accommodation_data_image/"
+        # self.accommodation_data = pd.read_csv("./data/accommodation_data.csv")
+        # restaurant_file_location = "C:/Users/KDT02/Desktop/new_team_project_1/image/restaurant_data_image/"
+        # self.restaurant_data = pd.read_csv("./data/restaurant_data.csv")
+        # tourist_attractions_location = "C:/Users/KDT02/Desktop/new_team_project_1/image/tourist_attractions_data_image/"
+        # self.tourist_attractions_data = pd.read_csv("./data/tourist_attractions_data.csv")
+
+        # # 숙박업 이미지 주소값 넣기
+        # image_address_list = list()
+        # for i in range(len(self.accommodation_data)):
+        #     image_address_list.append(f"{accommodation_file_location}{self.accommodation_data.loc[i, '업체명']}.png")
+        # self.image_address_data = pd.DataFrame(image_address_list, columns=["image_address"])
+        # self.accommodation_data = pd.concat([self.accommodation_data, self.image_address_data], axis=1)
+        # self.accommodation_data.to_csv("./data/accommodation_data.csv", index=False)
+
+        # # 음식점 이미지 주소값 넣기
+        # image_address_list = list()
+        # for i in range(len(self.restaurant_data)):
+        #     image_address_list.append(f"{restaurant_file_location}{self.restaurant_data.loc[i, '콘텐츠명']}.png")
+        # self.image_address_data = pd.DataFrame(image_address_list, columns=["image_address"])
+        # self.restaurant_data = pd.concat([self.restaurant_data, self.image_address_data], axis=1)
+        # self.restaurant_data.to_csv("./data/restaurant_data.csv", index=False)
+
+        # 음식점 이미지 주소값 넣기
+        # image_address_list = list()
+        # for i in range(len(self.tourist_attractions_data)):
+        #     image_address_list.append(f"{tourist_attractions_location}{self.tourist_attractions_data.loc[i, '여행지']}.png")
+        # self.image_address_data = pd.DataFrame(image_address_list, columns=["image_address"])
+        # self.tourist_attractions_data = pd.concat([self.tourist_attractions_data, self.image_address_data], axis=1)
+        # self.tourist_attractions_data.to_csv("./data/tourist_attractions_data.csv", index=False)
+
+        # ====================================================================================================================
+        # ERD에 맞춰서 컬럼 명 조정하기
+        # same_columns_name_list = ['id', 'brought', 'name', 'address', 'latitude', 'longitude', 'contact_number', 'detail', 'image_address']
+        #
+        # self.accommodation_data = pd.read_csv("./data/accommodation_data.csv")
+        # self.accommodation_data.columns = same_columns_name_list
+        # self.accommodation_data.to_csv("./data/accommodation_data.csv", index=False)
+        #
+        # self.restaurant_data = pd.read_csv("./data/restaurant_data.csv")
+        # self.restaurant_data.columns = same_columns_name_list
+        # self.restaurant_data.to_csv("./data/restaurant_data.csv", index=False)
+        #
+        #
+        # self.tourist_attractions_data = pd.read_csv("./data/tourist_attractions_data.csv")
+        # self.tourist_attractions_data.columns = same_columns_name_list
+        # self.tourist_attractions_data.to_csv("./data/tourist_attractions_data.csv", index=False)
+
+        # ====================================================================================================================
+        # ERD에 맞춰서 데이터프레임 분할하기.
+        # self.accommodation_data = pd.read_csv("./data/accommodation_data.csv")
+        # self.restaurant_data = pd.read_csv("./data/restaurant_data.csv")
+        # self.tourist_attractions_data = pd.read_csv("./data/tourist_attractions_data.csv")
+
+        # common_columns_list = ['id', 'brought', 'name', 'address', 'latitude', 'longitude', 'contact_number']
+        # self.accommodation_common_data = self.accommodation_data[common_columns_list]
+        # self.restaurant_common_data = self.restaurant_data[common_columns_list]
+        # self.tourist_attractions_data = self.tourist_attractions_data[common_columns_list]
+        #
+        # self.common_data = pd.concat([self.accommodation_common_data, self.restaurant_common_data, self.tourist_attractions_data], ignore_index=True)
+        # self.common_data.to_csv("./data/common_data.csv", index=False)
+
+        # ====================================================================================================================
+        # ERD에 맞춰서 데이터프레임 분할하기.
+        # self.accommodation_data = pd.read_csv("./data/accommodation_data.csv")
+        # self.restaurant_data = pd.read_csv("./data/restaurant_data.csv")
+        # self.tourist_attractions_data = pd.read_csv("./data/tourist_attractions_data.csv")
+        #
+        # common_columns_list = ['id', 'detail', 'image_address']
+        # # trip_place_sub_data
+        # # restaurant_sub_data
+        # # accommodation_sub_data
+        # self.accommodation_sub_data = self.accommodation_data[common_columns_list]
+        # self.accommodation_sub_data.to_csv("./data/accommodation_sub_data.csv", index=False)
+        #
+        # self.restaurant_sub_data = self.restaurant_data[common_columns_list]
+        # self.restaurant_sub_data.to_csv("./data/restaurant_sub_data.csv", index=False)
+        #
+        # self.trip_place_sub_data = self.tourist_attractions_data[common_columns_list]
+        # self.trip_place_sub_data.to_csv("./data/trip_place_sub_data.csv", index=False)
+        pass
+
+
+
+if __name__ == '__main__':
+    preprocess_table_object = PreprocessTable()
